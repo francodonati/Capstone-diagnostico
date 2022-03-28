b3a62e6321658990aa6b61e92e4d15bc8610a1f9
franco donati <fpdonati@uc.cl>
2022-03-28 20:46:42 -0300

    nueva funcionalidad

--- diagnostico.py
+++ diagnostico.py
@@ -19,12 +19,22 @@ def main():
     opcion = int(input("seleccione la funcion que desea ver: "))
     if opcion == 1:
         retweet10(df)
+    if opcion == 2:
+        users(df)
 
 
 def retweet10(df):
     d1 = df.sort_values(by='retweetCount', ascending=False)
     print(d1.head(10))
 
+def users(df):
+    d1 = df.user
+    d2 = []
+    for data in d1:
+        d2.append(data['username'])
+    df2 = pd.DataFrame(d2)
+    df2 = df2.value_counts(ascending=False)
+    print(df2.head(10))
 
 main()
 
